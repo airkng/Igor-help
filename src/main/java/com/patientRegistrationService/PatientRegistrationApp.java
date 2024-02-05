1f5b52d3ce7e3e96ec1ed00d5d0a7e2d89c83b87
Antonov A.S <java.dev.antonov.a.s@gmail.com>
2024-02-05 14:20:52 +0000

    fix: настройка проекта

--- src/main/java/com/patientRegistrationService/PatientRegistrationApp.java
+++ src/main/java/com/patientRegistrationService/PatientRegistrationApp.java
@@ -2,8 +2,10 @@ package com.patientRegistrationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication//(scanBasePackages = "com.patientRegistrationService")
+@ComponentScan(basePackages = {"com.patientRegistrationService"})
 public class PatientRegistrationApp {
 
 	public static void main(String[] args) {
--- src/main/java/com/patientRegistrationService/repository/PatientRepository.java
+++ src/main/java/com/patientRegistrationService/repository/PatientRepository.java
@@ -2,11 +2,12 @@ package com.patientRegistrationService.repository;
 
 import com.patientRegistrationService.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author MR.k0F31n
  */
-@Repository
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 }
